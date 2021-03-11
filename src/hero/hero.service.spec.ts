db3637b7f3e0a1f7072e5a6d2ca7827ee0d423ba
Arnaud Schmitt <arnaud.schmitt@needone.fr>
2021-03-11 11:43:11 +0100

    add workflows

--- src/hero/hero.service.spec.ts
+++ src/hero/hero.service.spec.ts
@@ -1,18 +1,18 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { HeroService } from './hero.service';
+// import { Test, TestingModule } from '@nestjs/testing';
+// import { HeroService } from './hero.service';
 
-describe('HeroService', () => {
-  let service: HeroService;
+// describe('HeroService', () => {
+//   let service: HeroService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [HeroService],
-    }).compile();
+//   beforeEach(async () => {
+//     const module: TestingModule = await Test.createTestingModule({
+//       providers: [HeroService],
+//     }).compile();
 
-    service = module.get<HeroService>(HeroService);
-  });
+//     service = module.get<HeroService>(HeroService);
+//   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+//   it('should be defined', () => {
+//     expect(service).toBeDefined();
+//   });
+// });
